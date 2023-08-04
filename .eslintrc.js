87968136ee4a67fc0428165c46a2593aef9924bd
Olga_Belaya <protenot@yandex.ru>
2023-08-04 12:24:13 +0300

    install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true
+    "jest/globals": true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: ["standard-with-typescript", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     tsconfigRootDir: __dirname,
-    root: true
+    root: true,
   },
-  plugins: ['@typescript-eslint', 'jest', 'import'],
+  plugins: ["@typescript-eslint", "jest", "import"],
   rules: {
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'no-undef': 'off',
-    'import/extensions': ['warn', 'never'],
-    '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off'
-  }
-}
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "no-undef": "off",
+    "import/extensions": ["warn", "never"],
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+  },
+};
