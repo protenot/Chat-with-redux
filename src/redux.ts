cdd760ec62c8cba16bee6192441b79a42196d114
Olga_Belaya <protenot@yandex.ru>
2023-08-06 12:09:05 +0300

    add store

--- src/redux.ts
+++ src/redux.ts
@@ -1,12 +1,28 @@
-/* import {createStore} from "redux";
-import {State, Action} from "./types"
+import { configureStore } from "@reduxjs/toolkit";
+import { State, Action } from "./types";
 function reducer(
-    state:State={name:"",messages:[]},
-    action:Action
+  state: State = { name: "", messages: [] },
+  action: Action,
+): State {
+  switch (action.type) {
+    case "SHOW_MESSAGES":
+      return {
+        ...state,
+        messages: [...state.messages, ...action.payload],
+      };
+    case "ADD_MESSAGE":
+      return {
+        ...state,
+        messages: [...state.messages, ...action.payload],
+      };
+    case "CREATE_USER":
+      return {
+        ...state,
+        name: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 
-): /* State
-{
-    switch(action.type){
-        case
-    }
-} */
+export const store = configureStore({ reducer });
